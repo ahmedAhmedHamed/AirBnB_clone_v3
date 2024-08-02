43b5fcd3b4adcf13ff6388927a4701174505d608
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-08-02 16:00:17 +0300

    (6) GET state/<ID>

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """default rest api actions for state objects.
     """
-from flask import jsonify
+from flask import abort
 from api.v1.views import app_views
 from models.engine.db_storage import classes
 from models import storage
@@ -16,4 +16,13 @@ def get_all_states():
     for _, state in all_states.items():
         ret.append(state.to_dict())
     return ret
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False)
+def get_specific_state(state_id):
+    """gets all available states and returns them as a dict"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return state.to_dict()
 
