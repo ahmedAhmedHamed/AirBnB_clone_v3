24a140d901314793f58b20eb47dcd89d261ce27d
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-08-02 14:22:27 +0300

    (4) stats endpoint working

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,20 @@
     """
 from flask import jsonify
 from api.v1.views import app_views
+from models.engine.db_storage import classes
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """ Returns the status of the application """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def all_object_count():
+    ret = {}
+    for class_name, class_type in classes.items():
+        count = storage.count(class_type)
+        ret[class_name] = count
+    return jsonify(ret)
