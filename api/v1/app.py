43324bd8c147f81dc89fe75fa2c23c07828c86d8
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-08-04 17:17:45 +0300

    reverted app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-""" default entry point for the application
+"""_summary_
     """
-from api.v1.views import app_views
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask
 from models import storage
+from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
-CORS(app, origins=["0.0.0.0"])
 
 app.register_blueprint(app_views)
 
@@ -19,14 +17,7 @@ def teardown(exception):
     storage.close()
 
 
-@app.errorhandler(404)
-def not_found(exception):
-    """ returns a 404 error"""
-    return jsonify({"error": "Not found"}), 404
-
-
 if __name__ == '__main__':
-    """launches the app"""
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
