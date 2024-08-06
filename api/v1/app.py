14f90a99be708ef84d89e5a97bdfdc7777d0a374
root <root@MSI>
2024-08-06 12:17:39 +0300

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" default entry point for the application
-    """
+"""
+default entry point for the application
+"""
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from flask_cors import CORS
