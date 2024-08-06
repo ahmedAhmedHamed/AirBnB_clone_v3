b55938c00678d10560d21bedccc8bd5fb4175b10
root <root@MSI>
2024-08-06 03:52:04 +0300

    air bnb

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,7 @@ from models import storage
 from os import getenv
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+# app.register_blueprint(app_views)
 # CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
