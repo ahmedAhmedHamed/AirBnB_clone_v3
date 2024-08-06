3e1633935c2138e760ec46fef5f33cd97e1bd862
root <root@MSI>
2024-08-06 08:09:19 +0300

    fix

--- check_docs.py
+++ check_docs.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python3
+"""Testing documentation of a module
+"""
 from importlib import import_module
 import sys
 
-try:
-    mod_to_check = import_module(sys.argv[1])
-    print(mod_to_check.__doc__)
-except ModuleNotFoundError as e:
-    print(e)
-    pass
+m_imported = import_module(sys.argv[1])
+
+if m_imported.__doc__ is None:
+    print("No module documentation", end="")
+else:
+    print("OK", end="")
